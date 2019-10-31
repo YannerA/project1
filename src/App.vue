9b1aa9da1bbb86aab4c3a57a23f2d36788e70672
YannerA <864239096@qq.com>
2019-10-31 23:22:01 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <home></home>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 引入首页组件
+import home from './views/home/index.vue'
+export default {
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    home
+  }
+}
+</script>
